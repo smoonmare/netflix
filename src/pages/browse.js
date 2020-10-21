b68479e85a953697e20f67fb6e3b482a7fb63758
smoonmare <smoonmare@gmail.com>
2020-10-21 16:28:42 -0700

    Added selectionFilter as a utility to be able to add Title to Slides without hardcoding it on the /browse

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useContent } from '../hooks';
+import { selectionFilter } from '../utils';
 
 export default function Browse() {
     const { series } = useContent('series');
     const { films } = useContent('films');
+    const slides = selectionFilter({ series, films });
+    console.log(slides);
     return <p>Hello from Browse page!</p>;
 }
