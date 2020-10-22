6b187d36e4d7a128b9d23fe2314b32a2f01856bf
smoonmare <smoonmare@gmail.com>
2020-10-22 16:42:31 -0700

    Added Header elements

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,49 @@
 import React from 'react';
 import { Link as ReactRouterLink } from 'react-router-dom';
-import { Background, Container, ButtonLink, Logo } from './styles/header';
+import { Background, Feature, FeatureCallOut, Dropdown, Group, Picture, Profile, Text, Link, Container, ButtonLink, Logo } from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children}</Background> : children;
 }
 
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+  return <Feature {...restProps}>{children}</Feature>;
+}
+Header.Picture = function HeaderPicture({ src, ...restProps}) {
+  return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+}
+
+Header.Profile = function HeaderProfile({ children, ...restProps}) {
+  return <Profile {...restProps}>{children}</Profile>;
+}
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
+}
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+}
+
+Header.TextLink = function HeaderTextLink({ children, ...restProps}) {
+  return <Link {...restProps}>{children}</Link>;
+}
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps}) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+}
+
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
-};
+}
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>;
+}
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
   return <ButtonLink {...restProps}>{children}</ButtonLink>;
-};
+}
 
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
   return (
