cf1b52abfc42ee0e22151c425afd1433c85e17e2
smoonmare <smoonmare@gmail.com>
2020-10-24 22:18:34 -0700

    Fixed search input bug

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -18,14 +18,16 @@ Header.Search = function HeaderSearch({ searchTerm, setSearchTerm, ...restProps 
 
   return (
     <Search {...restProps}>
-      <SearchIcon onClick={() => setSearchActive((searchActive) => !searchActive)}>
+      <SearchIcon onClick={() => setSearchActive((searchActive) => !searchActive)} data-testid="search-click">
         <img src="/images/icons/search.png" alt="Search" />
       </SearchIcon>
-      <SearchInput 
+      <SearchInput
         value={searchTerm}
-        onClick={({ target }) => setSearchTerm(target.value)}
-        placeholder="Search Catalog"
-        active={searchActive} />
+        onChange={({ target }) => setSearchTerm(target.value)}
+        placeholder="Search films and series"
+        active={searchActive}
+        data-testid="search-input"
+      />
     </Search>
   );
 }
